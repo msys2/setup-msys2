b3452aa847865e86d92d65d049cef96a52ab6418
eine <eine@users.noreply.github.com>
2020-01-19 23:05:20 +0100

    use 7z instead of tar

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ async function run() {
 
     const distrib = await tc.downloadTool('http://repo.msys2.org/distrib/x86_64/msys2-base-x86_64-20190524.tar.xz');
 
-    await exec.exec(`tar`, [ '-x', '-J', '--force-local', '-f', distrib ], {cwd: dest} );
+    await exec.exec(`bash`, ['-c', `7z x ${distrib.replace(/\\/g, '/')} -so | 7z x -aoa -si -ttar`], {cwd: dest} );
 
     let cmd = path.join(dest, 'msys2do.cmd');
     fs.writeFileSync(cmd, [
